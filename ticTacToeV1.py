2f4a4661784cd2093f824a3d0f5c7ababba11802
Jennifer Clark <jac300@nyu.edu>
2013-02-25 13:16:30 -0500

    added list comprehensions

--- ticTacToeV1.py
+++ ticTacToeV1.py
@@ -1,6 +1,8 @@
 from random import choice
-playerX = "player X"
-playerO = "player O"
+
+game_board_in_play = ["0", "1", "2", "3", "4", "5", "6", "7", "8"] 
+playerX = "Player X"
+playerO = "Player O"
 
 def welcome_to_new_game(playerX, playerO):
     print "Welcome to ticTacToe."
@@ -47,10 +49,6 @@ def verify_numerical_selection_for_move(user_move_input):
         return user_move_input
 
 def legal_move(current_board, current_move_selection):
-    # if current_board[current_move_selection] == "X" or current_board[current_move_selection] == "O":
-    #     return False
-    # else:
-    #     return True
     return current_board[current_move_selection].isdigit()
 
 def game_over(current_board):
@@ -63,7 +61,7 @@ def game_over(current_board):
 def there_is_a_winner(current_board):
     winning_combinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
     for pos1, pos2, pos3 in winning_combinations:
-        for mark in "XO":
+        for character in "XO":
             if current_board[pos1] == current_board[pos2] == current_board[pos3]:
                 return True
     return False
@@ -86,7 +84,7 @@ def handle_next_move(current_player, next_player):
         handle_next_move(current_player, next_player)
 
 def play_game(current_player, next_player, current_board):
-    #first time through
+    #first move
     display_board(current_board) 
     move_selection = raw_input ("Ok, %s, make your first move by selecting a number on the board \nto move to. \n" %current_player)
     verfied_move_selection = verify_numerical_selection_for_move(move_selection)
@@ -97,16 +95,7 @@ def play_game(current_player, next_player, current_board):
     handle_next_move(get_next_player(current_player, next_player), next_player)    
 
 def display_board(current_board):
-    # board = []
-    
-    # for item in current_board: 
-    #     if item is None:
-    #         board.append(" ")
-    #     else: 
-    #         board.append(item)
-
-    board = [x if x is not None else " " for x in current_board] #what is this syntax?
-    
+    board = [x if x else " " for x in current_board] #this is a list comprehension - making a new list based on performing operations on the items of an existing list
     print " {} | {} | {} ".format(board[0], board[1], board[2]) #using a format string, append the print statements with the new contents of board
     print "---+---+---"
     print " {} | {} | {} ".format(board[3], board[4], board[5])
@@ -116,7 +105,6 @@ def display_board(current_board):
 #program begins here
 first_player = welcome_to_new_game(playerX, playerO) 
 second_player = determine_second_player_identity(first_player)
-game_board_in_play = ["0", "1", "2", "3", "4", "5", "6", "7", "8"] 
 play_game(first_player, second_player, game_board_in_play) 
 
 
